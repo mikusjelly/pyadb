c532a2e75b4c99e519584f9e9bfe1fb4361569f7
mikusjelly <mikusjelly@gmail.com>
2017-11-30 16:04:35 +0800

    Supported python3

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 import os
 from distutils.core import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "pyadb",
-    version = "0.1.4",
-    author = "Chema Garcia",
-    author_email = "chema@safetybits.net",
-    description = ("Simple python module to interact with Android Debug Bridge tool"),
-    license = "BSD",
-    keywords = "python android adb",
-    url = "https://github.com/sch3m4/pyadb",
-    packages=['pyadb'],
+    name="pyadb3",
+    version="0.1.4",
+    author="mikusjelly",
+    author_email="mikusjelly@gmail.com",
+    description=(
+        "Simple python module to interact with Android Debug Bridge tool"),
+    license="BSD",
+    keywords="python android adb",
+    url="https://github.com/mikusjelly/pyadb3",
+    packages=['pyadb3'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 4 - Alpha",
         "Topic :: Utilities",
+        "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: BSD License",
     ],
 )
